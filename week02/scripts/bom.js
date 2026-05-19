05c4674b3661042386ad4c1fcbb287dc824655f3
damiandegree <damian.degree96@gmail.com>
2026-05-18 22:26:37 -0300

    .

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,10 +3,21 @@ const { createElement } = require("react");
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('______');
-const li = createElement('li');
-const deleteButton = createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+button.addEventListener("click",() => function() {
+    if (input.value != "") {
+      const li = createElement("li");
+      li.textContent = input.value;
+
+      const deleteButton = createElement("button");
+      deleteButton.textContent = "❌";
+      deleteButton.addEventListener("click", function () {
+      list.removeChild(li);
+      input.focus();
+    });
+      li.appendChild(deleteButton);
+      list.appendChild(li);
+      input.value = '';
+     }
+     input.focus();
+});
